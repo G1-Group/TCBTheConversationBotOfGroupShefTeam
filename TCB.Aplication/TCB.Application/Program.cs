7d191db182e70078526c54c1574a4f4dd62d6179
Malika Rasulova <malika-rasulova-2000@mail.ru>
2023-08-03 21:26:21 +0500

    delete

--- TCB.Aplication/TCB.Application/Program.cs
+++ TCB.Aplication/TCB.Application/Program.cs
@@ -1,11 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using TCB.Aplication.Service;
-using TCB.Aplication.Service.Interface;
+
 using Telegram.Bot;
 
-string token="6344298364:AAHi9gGdqWsd23bzNCEuDxMkS2OGDS8uyoc";
-TelegramBotClient client = new TelegramBotClient(token);
-IAnonymChatService anonymChatService = new AnonymChatService(client);
-anonymChatService.Initialize();
-Thread.Sleep(-1);

a2fafe695c447ef03fb5aebb527737f491b74095
Azimjon <azimjonshodiyorov@gmail.com>
2023-08-03 11:03:00 +0500

    Client modul  add

---
+++ TCB.Aplication/TCB.Application/Domain/Client.cs
@@ -0,0 +1,10 @@
+namespace TCB.Aplication.Domain;
+
+public class Client : ModelBase
+{
+    public long UserId { get; set; }
+    public long TelegramChatId { get; set; }
+    public string NickName { get; set; }
+    public bool IsPremium { get; set; }
+    public ClientStatus Status { get; set; }
+}

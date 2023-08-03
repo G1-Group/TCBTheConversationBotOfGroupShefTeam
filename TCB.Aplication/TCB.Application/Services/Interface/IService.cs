be0f72f4d616e3586b1ea24fe85965e0f471adb9
Malika Rasulova <malika-rasulova-2000@mail.ru>
2023-08-03 21:50:49 +0500

    intervace base

---
+++ TCB.Aplication/TCB.Application/Services/Interface/IService.cs
@@ -0,0 +1,14 @@
+using TCB.Aplication.Domain;
+
+namespace TCB.Aplication.Services.Interface;
+
+public interface  IService<T>  where T : ModelBase
+{
+    public void Add(T data);
+    public void Delete(T data);
+    public List<T> GetAllModel();
+    public T FindModel(Guid id);
+    public void AddRange(List<T> data);
+    
+ 
+}

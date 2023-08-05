9d37a7d3d0bf1bea7958433151ba091113ad04ca
Azimjon <azimjonshodiyorov@gmail.com>
2023-08-05 15:45:13 +0500

    SQL update

--- TCB.Aplication/TCB.Application/Infrastructure/Service/Querys/QueryUser.cs
+++ TCB.Aplication/TCB.Application/Infrastructure/Service/Querys/QueryUser.cs
@@ -2,25 +2,25 @@ namespace TCB.Aplication.Domain.Querys;
 
 public static class QueryUser
 {
-    public static string SelectQuery = $"SELECT * FROM SQL.User";
+    public static string SelectQuery = $"SELECT * FROM \"Telegrambot\".user";
     
-    public static string SelectByIdQuery = $"SELECT * FROM SQL.User WHERE id = @p0";
-    
-    
-    public static string SelectByUserIdQuery = $"SELECT * FROM SQL.User WHERE client_id = @p1";
+    public static string SelectByIdQuery = $"SELECT * FROM \"Telegrambot\".user WHERE id = @p0";
     
     
-    public static string SelectByPhoneNumberQuery = $"SELECT * FROM SQL.User WHERE phone_number = @p2";
+    public static string SelectByUserIdQuery = $"SELECT * FROM \"Telegrambot\".user WHERE client_id = @p1";
     
     
-    public static string SelectByPasswordQuery = $"SELECT * FROM SQL.User WHERE password = @p3";
+    public static string SelectByPhoneNumberQuery = $"SELECT * FROM \"Telegrambot\".user WHERE phone_number = @p2";
+    
+    
+    public static string SelectByPasswordQuery = $"SELECT * FROM \"Telegrambot\".user WHERE password = @p3";
 
 
-    public static string InsertQuery = $"INSERT INTO SQL.User (id , client_id , phone_number , password)" +
+    public static string InsertQuery = $"INSERT INTO \"Telegrambot\".user (id , client_id , phone_number , password)" +
                                        $" VALUES(@p0 , @p1 ,@p2 , @p3)";
 
     public static string UpdateQuery =
-        $"UPDATE SQL.User u SET id = @p0 , client_id = @p1 , phone_number = @p2 , password = @p3";
+        $"UPDATE \"Telegrambot\".user u SET id = @p0 , client_id = @p1 , phone_number = @p2 , password = @p3";
 
-    public static string DeleteQuery = $"DELETE FROM SQL.User WHERE id = @p0";
+    public static string DeleteQuery = $"DELETE FROM \"Telegrambot\".user WHERE id = @p0";
 }

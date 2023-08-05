415c157b61ff63b298a6b088af44754492f2880a
Ogabek1407 <137427263+Ogabek1407@users.noreply.github.com>
2023-08-05 16:45:09 +0500

    update MessageTCB

--- TCB.Aplication/TCB.Application/Domain/MessageTCB.cs
+++ TCB.Aplication/TCB.Application/Domain/MessageTCB.cs
@@ -2,10 +2,8 @@ namespace TCB.Aplication.Domain;
 
 public class MessageTCB:ModelBase
 {
-    public long? ChatId { get; set; }
     public string text { get; set; }
     public long FromId { get; set; }
     public DateTime time { get; set; }
-    public long? BoardId { get; set; }
-    
+    public long BoardId { get; set; }
 }

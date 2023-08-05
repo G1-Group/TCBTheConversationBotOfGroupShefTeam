8ad96f2ca46074195b217ca777dd1d439452e8b2
Ogabek1407 <137427263+Ogabek1407@users.noreply.github.com>
2023-08-05 22:37:23 +0500

    created ControllerManager

---
+++ TCB.Aplication/TCB.Application/TelegramBot/Managers/ControllerManager.cs
@@ -0,0 +1,16 @@
+namespace TCB.Aplication.TelegramBot.Managers;
+
+public class ControllerManager
+{
+    public ControllerManager()
+    {
+        
+    }
+
+    public ControllerBase GetControllerBySessionData(Session session)
+    {
+        //will check controllers by session.Conrtoller and return founded;
+        return null;
+    }
+}
+

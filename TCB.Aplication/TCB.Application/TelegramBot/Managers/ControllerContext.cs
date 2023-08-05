41198bef93232cde1bd37c024dc0ed2c5ae5c158
sobirxazratov <xazratovsobir3@gamil.com>
2023-08-05 16:14:19 -0700

    Create ControllerContext

---
+++ TCB.Aplication/TCB.Application/TelegramBot/Managers/ControllerContext.cs
@@ -0,0 +1,9 @@
+using Telegram.Bot.Types;
+
+namespace TCB.Aplication.TelegramBot.Managers;
+
+public class ControllerContext
+{
+    public Update Update { get; set; }
+    public Session Session { get; set; }
+}

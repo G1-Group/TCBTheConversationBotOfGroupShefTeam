918c9ffdfc69a04732f6241863bf4ee1a824bebb
Azimjon <azimjonshodiyorov@gmail.com>
2023-08-06 15:17:35 +0500

    add bug

--- TCB.Aplication/TCB.Application/DBQuerys/SQLQuerys.sql
+++ TCB.Aplication/TCB.Application/DBQuerys/SQLQuerys.sql
@@ -1,52 +1,50 @@
-create schema TelegramBot;
+create schema if not exists TelegramBot;
 
 
-create table  "telegrambot".user (
-                                     id serial primary key,
-                                     client_id bigint ,
-                                     password varchar(20),
-                                     phone_number varchar(20)
-);
-
-
-
-
-create table "telegrambot".Client (
-                                      id serial primary key ,
-                                      user_id bigint  REFERENCES "telegrambot".user(id),
-                                      chat_id int,
-                                      nickName varchar(20),
-                                      isPremium bool,
-                                      status int,
-                                      clientInAnonymChat bool
-);
-
-
-
-
-
-create table "telegrambot".Board (
-                                     id serial primary key ,
-                                     owner_id bigint references "telegrambot".Client(id),
-                                     nickName varchar(20),
-                                     to_id bigint REFERENCES "telegrambot".Client(id))
+create table if not exists  "telegrambot".user (
+    id bigserial primary key,
+    client_id bigint ,
+    password varchar(20),
+    phone_number varchar(20)
     );
 
 
-create table "telegrambot".AnonyChat(
-                                        id serial primary key ,
-                                        client_id int,
-                                        status int,
-                                        create_time timestamp ,
-                                        from_id bigint REFERENCES "telegrambot".Client(id),
-                                        to_id bigint REFERENCES "telegrambot".Client(id)
-);
 
-create table "telegrambot".Message(
-                                      id int ,
-                                      message varchar(200),
-                                      from_id int REFERENCES "telegrambot".Client(id),
-                                      chat_id bigint REFERENCES "telegrambot".AnonyChat(id)
-                                          board_id int,
 
-);
+create table if not exists "telegrambot".Client (
+    id bigserial primary key ,
+    user_id bigint  REFERENCES "telegrambot".user(id),
+    chat_id bigint,
+    nickName varchar(20),
+    isPremium bool,
+    status int,
+    client_in_anonymChat bool
+    );
+
+
+
+create table if not exists "telegrambot".Board (
+    id bigserial primary key ,
+    owner_id bigint references "telegrambot".Client(id),
+    nickName varchar(20)
+    );
+
+
+create table if not exists "telegrambot".AnonyChat(
+    id bigserial primary key ,
+    status int,
+    create_time date ,
+    from_id bigint REFERENCES "telegrambot".Client(id),
+    to_id bigint REFERENCES "telegrambot".Client(id)
+    );
+
+create table if not exists "telegrambot".Message(
+    id bigserial primary key ,
+    message varchar(200),
+    from_id bigint,
+    chat_id bigint ,
+    board_id bigint,
+    message_status int
+    );
+
+

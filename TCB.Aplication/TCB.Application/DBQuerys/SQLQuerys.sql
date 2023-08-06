1e8237f23f79d05ceaf515dfdbee7312ad5a3d17
Azimjon <azimjonshodiyorov@gmail.com>
2023-08-06 14:44:02 +0500

    Created SQL querys

--- TCB.Aplication/TCB.Application/DBQuerys/SQLQuerys.sql
+++ TCB.Aplication/TCB.Application/DBQuerys/SQLQuerys.sql
@@ -1,16 +1,51 @@
-create table users(id serial primary key,telegram_client_id bigint,phone_number varchar(13),password varchar(16));
-
-create table clients(client_id serial primary key, user_id bigint REFERENCES Users(id),
-first_name varchar(30),user_name varchar(30),nickname varchar(20),status int,isPremium bool);
-
-create table anonym_chats (chat_id serial primary key,state int,created_date timestamp,from_id bigint REFERENCES clients(client_id),
-to_id bigint REFERENCES clients(client_id));
+create schema TelegramBot;
 
 
-create table boards(board_id serial primary key,nickname varchar(15), owner_id bigint references clients(client_id));
+create table  "telegrambot".user (
+                                     id serial primary key,
+                                     client_id bigint ,
+                                     password varchar(20),
+                                     phone_number varchar(20)
+);
 
-create table messages(id serial primary key,from_id bigint REFERENCES clients(client_id),message varchar(400),
-chat_id bigint REFERENCES anonym_chats(chat_id),
-board_id bigint REFERENCES boards(board_id),message_type int);
 
-create table logs(id serial primary key,from_id bigint,to_id bigint,action varchar(100));
+
+
+create table "telegrambot".Client (
+                                      id serial primary key ,
+                                      user_id bigint  REFERENCES "telegrambot".user(id),
+                                      chat_id int,
+                                      nickName varchar(20),
+                                      isPremium bool,
+                                      status int,
+                                      clientInAnonymChat bool
+);
+
+
+
+create table "telegrambot".Board (
+                                     id serial primary key ,
+                                     owner_id bigint references "telegrambot".Client(id),
+                                     nickName varchar(20),
+                                     to_id bigint REFERENCES "telegrambot".Client(id))
+    );
+
+
+create table "telegrambot".AnonyChat(
+                                        id serial primary key ,
+                                        client_id int,
+                                        status int,
+                                        create_time timestamp ,
+                                        from_id bigint REFERENCES "telegrambot".Client(id),
+                                        to_id bigint REFERENCES "telegrambot".Client(id)
+);
+
+create table "telegrambot".Message(
+                                      id int ,
+                                      message varchar(200),
+                                      from_id int REFERENCES "telegrambot".Client(id),
+                                      chat_id bigint REFERENCES "telegrambot".AnonyChat(id)
+                                          board_id int,
+
+);
+e logs(id serial primary key,from_id bigint,to_id bigint,action varchar(100));

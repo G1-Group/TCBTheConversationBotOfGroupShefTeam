8443a429a99bfc65d7f6a67085da203d43de2695
Malika Rasulova <malika-rasulova-2000@mail.ru>
2023-08-08 20:27:41 +0500

    interface AnonymChat added

--- TCB.Aplication/TCB.Application/Services/Interface/IAnonymChatService.cs
+++ TCB.Aplication/TCB.Application/Services/Interface/IAnonymChatService.cs
@@ -5,4 +5,11 @@ namespace TCB.Aplication.Services.Interface;
 public interface IAnonymChatService : IService<AnonymChat>
 {
     
+    public  Task<AnonymChat> Add(AnonymChat data);
+    public  Task<AnonymChat> Delete(AnonymChat data);
+    public  Task<AnonymChat> Update(long Id, AnonymChat data);
+    public  Task<List<AnonymChat>> GetAllModel();
+    public  Task<AnonymChat> FindById(long Id);
+    public  Task<AnonymChat> FindByIdModel(long Id);
+    public Task<AnonymChat> FindByNickNameModel(string nickName);
 }
